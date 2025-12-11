324814f045c9c4693f893f80e6582d8bc53a45a7
chenkianwee <chenkianwee@gmail.com>
2025-12-11 17:43:20 -0500

    version 0.0.2 added gen rays

--- src/raytrace_mrt_lib/__init__.py
+++ src/raytrace_mrt_lib/__init__.py
@@ -25,3 +25,29 @@ def separate_rays(rays: list[geomie3d.utility.Ray], nparallel: int):
         end = int(interval*(i+1))
         rays_ls.append(rays[start:end])
     return rays_ls
+
+def gen_rays(grid_xyzs: list[list[float]], ndirs: int) -> list[geomie3d.utility.Ray]:
+    """
+    generate rays for each mrt point
+
+    Parameters
+    ----------
+    grid_xyzs : list[list[float]]
+        list[shape(npts, 3)] the mrt grids to calc the MRT.
+
+    ndirs: int
+        The number of rays for each grid point.
+
+    Returns
+    -------
+    list[geomie3d.utility.Ray]
+        the rays generated for projection. Each ray has an attribute grid_id to identify it for later mrt calculations.
+    """
+    unitball = geomie3d.d4pispace.tgDirs(ndirs)
+    #create the rays for each analyse pts
+    rays = []
+    for cnt,xyz in enumerate(grid_xyzs):
+        for dix in unitball.getDirList():
+            ray = geomie3d.create.ray(xyz, [dix.x, dix.y, dix.z], attributes = {'grid_id': cnt})        
+            rays.append(ray)
+    return rays
